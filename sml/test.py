031445f031be1b47cf1936ac66a1ed81fdfdde3e
SimonDahl <s162695@student.dtu.dk>
2022-09-12 10:42:22 +0200

    test test

--- sml/test.py
+++ sml/test.py
@@ -4,5 +4,8 @@ A = np.array([1])
 
 print(A)
 
+print('Hej Andreas virker det her?')
+
+
 
 # pip install -r C:\Users\Ejer\OneDrive - Danmarks Tekniske Universitet\Dokumenter\GitHub\Speciale\requirements.txtpi
