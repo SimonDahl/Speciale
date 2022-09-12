dbdbbad15db2ffc6cabe792d303dc6466d4c7438
SimonDahl <s162695@student.dtu.dk>
2022-09-12 09:50:35 +0200

    prøv lige det her ven

--- sml/test.py
+++ sml/test.py
@@ -1,8 +1,8 @@
 import numpy as np 
 
-A = np.array([1,2,3])
+A = np.array([1])
 
 print(A)
 
 
-
+# pip install -r C:\Users\Ejer\OneDrive - Danmarks Tekniske Universitet\Dokumenter\GitHub\Speciale\requirements.txtpi
