2a54d6ff0ba9cfa82d2b750ca08757b1a5962447
SimonDahl <s162695@student.dtu.dk>
2022-11-14 11:27:53 +0100

    med example

--- Code/PI_GANs/PID_GAN_example.py
+++ Code/PI_GANs/PID_GAN_example.py
@@ -1,4 +1,4 @@
-# er lavet helt som i artiklen, virker men generer samme løsning 
+# er lavet helt som i artiklen, har både boundary points og tager kun et punkt af gangen som input. ser ikke ud til at lære noget som helst 
 
 # prerequisites
 from ast import arg
@@ -23,8 +23,8 @@ from scipy.integrate import odeint, solve_ivp
 
 n_data = 5
 bs = 1
-time_limit = 6
-n_col = 2000
+time_limit = 5
+n_col = 3000
 
 
 
@@ -37,7 +37,7 @@ x_dim = 1
 y_dim = 1 
 criterion = nn.BCELoss() 
 criterion_mse = nn.MSELoss()
-n_epochs = 3000
+n_epochs = 2000
 
 gen_epoch = 5
 lambda_phy = 1
@@ -55,37 +55,36 @@ m = 1
 k = 5
 
 #for i in range(n_data):
- #   y_b[i] = 3
+ #   y_b[i] = np.random.uniform(3,5)
+    #y_b[i] = 3
     
 def sho(t,y):
     solution = (y[1],(-m*y[1]-k*y[0]))
     return solution
     
-
-y_init = [3,1]
-solution = solve_ivp(sho, [0,timesteps], y0 = y_init, t_eval = t)
-sol_data = solution.y[0]
+  
+for i in range(1):
+    y_init = [3,1]
+    solution = solve_ivp(sho, [0,timesteps], y0 = y_init, t_eval = t)
+    sol_data = solution.y[0]
     
 sol_plot = np.array([sol_data])  
 
 y_b = [sol_data[0],sol_data[5],sol_data[49],sol_data[70],sol_data[105]]
   
 y_b = np.array([y_b])
+
   
-  
+x_col = np.linspace(0, time_limit, n_col)
+
 x_b = [t[0],t[5],t[49],t[70],t[105]]
 x_b = np.array([x_b])
 
-
-x_col = np.linspace(0, time_limit, n_col)
-
-#x_b = np.zeros([n_data])
 t_sample = t.reshape(timesteps,1)
-
 #Xmean, Xstd = x_col.mean(0), x_col.std(0)
 #x_col = (x_col - Xmean) / Xstd
 #x_b = (x_b - Xmean) / Xstd
-#X_star_norm = (t_sample - Xmean) / Xstd
+#X_star_norm = (t_sample - Xmean) / Xstd 
 
 x_b = Variable(torch.from_numpy(x_b).float(), requires_grad=True).to(device)
 y_b = Variable(torch.from_numpy(y_b).float(), requires_grad=True).to(device)
@@ -94,11 +93,9 @@ x_b = x_b.reshape(n_data,1)
 
 
 
-#X_star_norm = Variable(torch.from_numpy(X_star_norm).float(), requires_grad=True).to(device)
 X_star_norm = Variable(torch.from_numpy(t_sample).float(), requires_grad=True).to(device)
 x_col = x_col.reshape(n_col,1)
 x_col = Variable(torch.from_numpy(x_col).float(), requires_grad=True).to(device)
-
 
 
 
@@ -109,8 +106,7 @@ class Generator(nn.Module):
         self.fc1 = nn.Linear(g_input_dim, n_neurons)
         self.fc2 = nn.Linear(n_neurons, n_neurons)
         self.fc3 = nn.Linear(n_neurons, g_output_dim)
-    
-        
+            
         
     # forward method
     def forward(self,y):
@@ -153,7 +149,7 @@ class Q_net(nn.Module):
 
 # build network
 G = Generator(g_input_dim = z_dim+x_dim, g_output_dim = 1).to(device)
-D = Discriminator(x_dim+y_dim).to(device)
+D = Discriminator(x_dim+y_dim+1).to(device)
 Q = Q_net(x_dim+y_dim,1)
 
 
@@ -165,23 +161,17 @@ Q_optimizer = optim.Adam(Q.parameters(), lr=lr)
 
 # Physics-Informed residual on the collocation points         
 def compute_residuals(x,u):
+    
+   
     #z = Variable(torch.randn(z_dim).to(device))
                
     u_t  = torch.autograd.grad(u, x, torch.ones_like(u), retain_graph=True,create_graph=True)[0]# computes dy/dx
     u_tt = torch.autograd.grad(u_t,  x, torch.ones_like(u_t),retain_graph=True ,create_graph=True)[0]# computes d^2y/dx^2
-    
-    
-    #m = np.random.uniform(1,5)
-   #k = np.random.uniform(1,5)
        
     r_ode = u_tt + m*u_t + k*u# computes the residual of the 1D harmonic oscillator differential equation
-    
     #r_ode = m*u_tt + k*u
-    
-    #r_ode = k*u-u_t 
-    
-    
-       
+     
+    #r_ode = k*u-u_t   
     return r_ode
 
 
@@ -190,37 +180,51 @@ def n_phy_prob(x):
     g_input = torch.cat((x,noise),dim=1)
     u = G(g_input)
     res = compute_residuals(x,u)
-    #n_phy = torch.exp(-lambda_val * (res**2))
+    n_phy = torch.exp(-lambda_val * (res**2))
         
-    return u,noise,res
+    return u,noise,n_phy,res
 
-def discriminator_loss(logits_real_u, logits_fake_u):
-        loss =  - torch.mean(torch.log(1.0 - torch.sigmoid(logits_real_u) + 1e-8) + torch.log(torch.sigmoid(logits_fake_u) + 1e-8)) 
+def discriminator_loss(logits_real_u, logits_fake_u, logits_fake_f, logits_real_f):
+        loss =  - torch.mean(torch.log(1.0 - torch.sigmoid(logits_real_u) + 1e-8) + torch.log(torch.sigmoid(logits_fake_u) + 1e-8)) \
+                - torch.mean(torch.log(1.0 - torch.sigmoid(logits_real_f) + 1e-8) + torch.log(torch.sigmoid(logits_fake_f) + 1e-8))
         return loss
 
-def generator_loss(logits_fake_u):
-        gen_loss = torch.mean(logits_fake_u)
-        return gen_loss
+def generator_loss(logits_fake_u, logits_fake_f):
+    gen_loss = torch.mean(logits_fake_u) + torch.mean(logits_fake_f)
+    return gen_loss
     
     
 def D_train(x,y_train):
     
     D_optimizer.zero_grad()
      
-    
+    real_prob = torch.ones_like(x)
+      
     # real y value for Discriminator  
    
-    d_input = torch.cat((x,y_train),dim=1)
+   
+    d_input = torch.cat((x,y_train,real_prob),dim=1)
     real_logits = D(d_input)
     
         
     # physics loss for boundary point 
-    u,_,_ = n_phy_prob(x)
-   
-    fake_logits_u = D(torch.cat((x,u),dim=1))
+    u,_,n_phy,_ = n_phy_prob(x)
+
+
+    fake_logits_u = D(torch.cat((x,u,n_phy),dim=1))
     
-   
-    D_loss = discriminator_loss(real_logits, fake_logits_u)
+    
+    # physics loss for collocation points 
+    
+    u_col,_,n_phy_col,_ = n_phy_prob(x_col)
+    fake_logits_col = D(torch.cat((x_col,u_col,n_phy_col),dim=1))
+
+    # computing synthetic real logits on collocation points for discriminator loss
+
+    real_prob_col = torch.ones_like(x_col)
+    real_logits_col = D(torch.cat((x_col,u_col,real_prob_col),dim=1))
+    
+    D_loss = discriminator_loss(real_logits,fake_logits_u,fake_logits_col,real_logits_col)
     D_loss.backward(retain_graph=True)
     D_optimizer.step()
     return D_loss.data.item()
@@ -236,26 +240,23 @@ def G_train(x,y_train):
         
         # u,noise,n_phy,res
         
-        _,_,phyloss1  = n_phy_prob(x_col)
-        
+        u_col,_,n_phy,phyloss_1  = n_phy_prob(x_col)
+        fake_logits_col = D(torch.cat((x_col,u_col,n_phy),dim=1))
 
         # physics loss for boundary points 
         
-        y_pred,G_noise,_ = n_phy_prob(x)
-        fake_logits_u = D(torch.cat((x,y_pred),dim=1))
+        y_pred,G_noise,n_phy,phyloss2 = n_phy_prob(x)
+        fake_logits_u = D(torch.cat((x,y_pred,n_phy),dim=1))
 
         z_pred = Q(torch.cat((x,y_pred),dim=1))
         mse_loss_z = criterion_mse(z_pred,G_noise)
 
         mse_loss = criterion_mse(y_pred,y_train)
         
-        adv_loss = generator_loss(fake_logits_u)
+        adv_loss = generator_loss(fake_logits_u,fake_logits_col)
         
-        phy_loss = (phyloss1**2).mean()
-        
-        G_loss = adv_loss + lambda_phy * phy_loss + lambda_q * mse_loss_z
-        
-        
+        G_loss = adv_loss + lambda_q* mse_loss_z +mse_loss/n_data
+
         G_loss.backward(retain_graph=True)
         G_optimizer.step()
 
@@ -302,16 +303,7 @@ for epoch in range(1, n_epochs+1):
 #plt.plot(t,u_plot)
 #plt.show() 
 
-""" t_test = np.linspace(0, time_limit, n_col)
-t_test = t_test.reshape(n_col,1)
-t_test = Variable(torch.from_numpy(t_test).float(), requires_grad=True).to(device)
 
-
-res,res_plot = compute_residuals(t_test)
-t_plot = t_test.cpu().detach().numpy()
-plt.plot(t_plot,res_plot)
-plt.show()
- """
 with torch.no_grad():
     
     
@@ -323,6 +315,3 @@ with torch.no_grad():
     plt.plot(t,sol_data)
     plt.scatter(x_b,y_b,color='red')
     plt.show()
-
-    
-    
